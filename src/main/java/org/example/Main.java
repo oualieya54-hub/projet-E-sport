5dc74d12c93b76c2d5245d6c108add790ab8415f
Hergli-omar <hergliomarh@gmail.com>
2026-05-07 02:05:16 +0100

    add hover effects, unify Game Pilot theme, update Main startup

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,33 +13,18 @@ public class Main {
     public static class EsportApp extends Application {
         @Override
         public void start(Stage primaryStage) throws Exception {
-            // Load FormationView
-            URL formationUrl = getClass().getResource("/FormationView.fxml");
+            URL fxmlUrl = getClass().getResource("/AcademyMainView.fxml");
             
-            if (formationUrl == null) {
-                System.err.println("ERREUR : Impossible de trouver le fichier FormationView.fxml !");
+            if (fxmlUrl == null) {
+                System.err.println("ERREUR : Impossible de trouver le fichier AcademyMainView.fxml !");
                 System.exit(1);
             }
 
-            Parent formationRoot = FXMLLoader.load(formationUrl);
-            Scene formationScene = new Scene(formationRoot);
-            primaryStage.setTitle("Game Pilot - Formation");
-            primaryStage.setScene(formationScene);
+            Parent root = FXMLLoader.load(fxmlUrl);
+            Scene scene = new Scene(root);
+            primaryStage.setTitle("Game Pilot - Academy & Coaching");
+            primaryStage.setScene(scene);
             primaryStage.show();
-
-            // Load SessionView
-            URL sessionUrl = getClass().getResource("/SessionView.fxml");
-            
-            if (sessionUrl == null) {
-                System.err.println("ERREUR : Impossible de trouver le fichier SessionView.fxml !");
-            } else {
-                Parent sessionRoot = FXMLLoader.load(sessionUrl);
-                Scene sessionScene = new Scene(sessionRoot);
-                Stage sessionStage = new Stage();
-                sessionStage.setTitle("Game Pilot - Session");
-                sessionStage.setScene(sessionScene);
-                sessionStage.show();
-            }
         }
     }//
 
