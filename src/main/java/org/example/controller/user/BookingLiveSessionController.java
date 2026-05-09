487248f3e0193db669678d4e211c727ddaf5391c
Hergli-omar <hergliomarh@gmail.com>
2026-05-09 23:47:36 +0100

    fix(user): add missing Event import to controllers for consistent navigation

--- src/main/java/org/example/controller/user/BookingLiveSessionController.java
+++ src/main/java/org/example/controller/user/BookingLiveSessionController.java
@@ -6,6 +6,7 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
--- src/main/java/org/example/controller/user/UserDashboardController.java
+++ src/main/java/org/example/controller/user/UserDashboardController.java
@@ -3,6 +3,7 @@ package org.example.controller.user;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
