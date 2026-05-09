d735587adab760a7c42795413e686a03258c9f61
Hergli-omar <hergliomarh@gmail.com>
2026-05-09 22:06:42 +0100

    fix(controller): correct Stage import in VisionConferenceController

--- src/main/java/org/example/controller/user/VisionConferenceController.java
+++ src/main/java/org/example/controller/user/VisionConferenceController.java
@@ -8,7 +8,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.Stage;
+import javafx.stage.Stage;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import org.example.Model.Booking;
